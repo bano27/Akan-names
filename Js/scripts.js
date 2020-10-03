5750e265d7cdc656fb84f04f826775fca77548b6
bano27 <thairukibe2798@gmail.com>
2020-10-03 18:30:52 +0300

    running js script file

---
+++ Js/scripts.js
@@ -0,0 +1,31 @@
+console.log(document.getElementById('birthdays'));
+var bday;
+bday = window.prompt("When is your birthday (YYYY-MM-DD)?","");
+var bdayArray = bday.split('-');
+
+
+//validation
+if(bdayArray.length !== 3){
+     alert("invalid Date");
+}else{
+    if(!bdayArray[0].match(/^\d\d\d\d$/) ||
+       !bdayArray[1].match(/^\d\d$/) ||
+       !bdayArray[2].match(/^\d\d$/)){
+        alert("invalid Date");
+    }else{
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];
+        var currentTime = new Date(
+            parseInt(bdayArray[0]),
+            parseInt(bdayArray[1]) - 1, //month starts from 0
+            parseInt(bdayArray[2])
+        );
+        var currentDay = currentTime.getDay();
+        var currentDayName= days[currentDay];
+alert ("You were born on " + currentDayName);
+    }
+}
+
+//submit button style
+
+const btn = document.queryselector('.btn');
+btn.style.background = 'red';
